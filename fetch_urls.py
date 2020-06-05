27d657c47dbd5807fe3094ffc8be3e98cfa9506a
sir_polin <polin@phystech.edu>
2020-06-05 20:24:27 +0300

    SQL support added, without concurrency

--- fetch_urls.py
+++ fetch_urls.py
@@ -1,30 +1,88 @@
+#!/usr/bin/python3
+
+# ! pip3 install beautifulsoup4, lxml
 import bs4
+# ! pip3 install requests
 import requests
+# ! pip3 install sqlalchemy, psycopg2
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, func
+
+# standard library modules
 import re
+import math
 from datetime import datetime
-import time
-from pandas import DataFrame as df
 
-resp_pattern = re.compile(r'<a href="/services/responses/bank/response/\d+/')
-supalen = len('<a href="/services/responses/bank/response/')
+from response import Response
 
-ids = []
+with open('_product_names', 'r') as product_names_file:
+    product_names = [line.rstrip('\n') for line in product_names_file]
 
-def fetch_url(url):
-    while(1):
-        try:
-            resp = requests.get(url)
-        except:
-            print('sleep', page, 'th')
-            time.sleep(10)
-            continue
-        else:
-            break
-    result = resp_pattern.findall(resp.text)
-    #print(tmp_result)
-    ids.extend([int(elem[supalen:-1]) for elem in result])
+# _db_string pattern:
+# postgres://user:password@server:port/table_name
+with open('_db_string', 'r') as db_string_file:
+    db_string = db_string_file.readline()
 
-def fetch_urls(urls):
-    for url in urls:
-        fetch_url(url)
-        #print('Fetching ', url)
+engine = create_engine(db_string, pool_recycle=60)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def fetch_product(product_name, last_id):
+    with open('_resp_suffix', 'r') as resp_suffix_file:
+        resp_suffix_str = resp_suffix_file.readline()
+
+    resp_pattern_href = f'<a href="/{resp_suffix_str}/'
+    resp_pattern_regexp = re.compile(fr'{resp_pattern_href}\d+/')
+    pattern_prefix_len = len(resp_pattern_href)
+
+    with open('_site_prefix', 'r') as site_prefix_file:
+        site_prefix_str = site_prefix_file.readline()
+
+    with open('_product_suffix', 'r') as product_suffix_file:
+        product_suffix_str = product_suffix_file.readline()
+
+    url = f'{site_prefix_str}/{product_suffix_str}/{product_name}/'
+    resp = requests.get(url)
+    soup = bs4.BeautifulSoup(resp.text, 'lxml')
+    count_responses = int(soup.find('div', 'margin-top-default').attrs['data-options'].split(';')[2].split(':')[1])
+    print('responses=', count_responses)
+    total_pages = math.ceil(int(count_responses) / 25)
+    print('pages=', total_pages)
+
+    for page in range(total_pages):
+        tmp_url = f'{site_prefix_str}/{product_suffix_str}/{product_name}/?page={page + 1}'
+        tmp_resp = requests.get(tmp_url)
+        tmp_result = resp_pattern_regexp.findall(tmp_resp.text)
+        # removing duplicates
+        del tmp_result[::2]
+        for elem in tmp_result:
+            tmp_id = int(elem[pattern_prefix_len:-1])
+            if tmp_id < last_id:
+                continue
+            __response = session.query(Response).filter_by(response_id=tmp_id).first()
+            try:
+                # update dynamic response fields
+                setattr(__response, product_name, True)
+            except AttributeError:
+                # getting all available response data
+                session.add(Response(tmp_id, product_name))
+
+            session.commit()
+
+
+def fetch_products():
+    for current_product_name in product_names:
+        # get last stored id from database
+        max_current_id = session.query(func.max(Response.response_id)).filter_by(**{current_product_name: True}).scalar()
+        if max_current_id is None:
+            max_current_id = 10041337
+            # default value for 2017 year
+        fetch_product(current_product_name, max_current_id)
+
+#for examlpe, getting of leasing
+start = datetime.now()
+fetch_product('leasing', 370938)
+end = datetime.now()
+print(end-start)
+
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-# ! pip3 install beautifulsoup4
+# ! pip3 install beautifulsoup4, lxml
 import bs4
 # ! pip3 install requests
 import requests
 # ! pip3 install pandas
 from pandas import DataFrame
+# ! pip3 install sqlalchemy, psycopg2
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, func
 
 # standard library modules
-import json
 import logging
-import re
 import time
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
@@ -19,13 +20,14 @@ import sys
 import subprocess
 import pickle
 
+from response import Response
+
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--verbose', '-v', action='store_true')
 arg_parser.add_argument('--debug', '-d', action='store_true')
 arg_parser.add_argument('--log', nargs='?', type=str, default=None)
 arg_parser.add_argument('--min-id', type=int, default=10041337)  # 2017 year
-arg_parser.add_argument('--input', '-i', type=argparse.FileType('r'), required=True)
-arg_parser.add_argument('--output', '-o', type=argparse.FileType('wb'), required=True)
+# arg_parser.add_argument('--output', '-o', type=argparse.FileType('wb'), required=True)
 arg_parser.add_argument('--max-count', '-c', type=int, default=None)
 arg_parser.add_argument('--gzip-output', '-z', action='store_true')
 arguments = arg_parser.parse_args(sys.argv[1:])
@@ -47,52 +49,19 @@ class MyException(Exception):
     pass
 
 
-all_product_types = set()
+with open('_resp_suffix', 'r') as resp_suffix_file:
+    resp_suffix_str = resp_suffix_file.readline()
+
+with open('_site_prefix', 'r') as site_prefix_file:
+    site_prefix_str = site_prefix_file.readline()
+
+# _db_string pattern:
+# postgres://user:password@server:port/table_name
+    with open('_db_string', 'r') as db_string_file:
+        db_string = db_string_file.readline()
 
 
-def parse_product(soup, url):
-    scripts = soup.findAll('script')
-    matching_script = None
-    for script in scripts:
-        if "allPagesHeader2" in script.text:
-            matching_script = script
-            break
-    if matching_script is None:
-        logging.error("%s document has no 'allPagesHeader2' data in <script>", url)
-        raise MyException()
-    script_parts = re.split(r'DFP.default\(document,', matching_script.text)
-    if len(script_parts) < 2:
-        logging.error("%s document has no 'DFP.default(document,' data in <script>", url)
-        raise MyException()
-    script_tail = script_parts[1]
-    script_parts = re.split(r'\);', script_tail, maxsplit=1)
-    all_pages_header_2_text = script_parts[0]
-    try:
-        json_data = json.loads(all_pages_header_2_text)
-    except Exception as e:
-        logging.error("%s document has no JSON data after 'DFP.default(document,' data in <script>", url)
-        raise MyException()
-    if 'allPagesHeader2' not in json_data:
-        logging.error("%s document has no 'allPagesHeader2' in JSON data in <script>", url)
-        raise MyException()
-    all_pages_header_2 = json_data['allPagesHeader2']
-    if 'targets' not in all_pages_header_2:
-        logging.error("%s document has no 'targets' in JSON['allPagesHeader2'] data in <script>", url)
-        raise MyException()
-    targets = all_pages_header_2['targets']
-    ans = ()
-    for elem in targets:
-        if elem['name'] == 'NRproduct':
-            if type(elem['value']) == list:
-                ans = set(elem['value'])
-            else:
-                ans = set([elem['value']])
-    global all_product_types
-    all_product_types |= set(ans)
-    return ans
-
-
-def get_responce_by_id(response_id):
+def get_response_by_id(response_id):
     tag_bank_name = 'header-h2 display-inline margin-right-x-small'
     tag_city = 'response-page__bank-meta font-size-medium color-gray-burn'
     tag_title = 'header-h0 response-page__title'
@@ -100,33 +69,24 @@ def get_responce_by_id(response_id):
     tag_num_views = 'icon-font icon-eye-16 icon-font--size_small'
     tag_num_comments = 'link-with-icon__text color-gray-blue--alpha-60'
 
-    # dynamic data extracting
-    # 'status': mark_tag[-1].text.strip('\n\t')
-    # 'num_views': int(soup.find('span', tag_num_views).next_element)
-    # 'num_comments': int(soup.find('span', tag_num_comments).text)
-
-    url = 'https://www.banki.ru/services/responses/bank/response/{}/'.format(response_id)
-    # print(url)
+    url = f'{site_prefix_str}/{resp_suffix_str}/{response_id}/'
     while True:
         try:
             resp = requests.get(url)
             if resp.status_code == 404:
-                logging.info('in {} resp.status_code was 404 with {}'.format(datetime.now().time(), response_id))
+                logging.info(f'in {datetime.now().time()} resp.status_code was 404 with {response_id}')
                 return
             if resp.status_code != 200:
                 logging.error("Error getting %s: %d", url, resp.status_code)
                 return
             soup = bs4.BeautifulSoup(resp.text, 'lxml')
             if soup.find('h1', 'header-h0 margin-bottom-large') is not None:
-                logging.info(
-                    'in {} header-h0 margin-bottom-large was not None {}'.format(datetime.now().time(), response_id))
+                logging.info(f'in {datetime.now().time()} header-h0 margin-bottom-large was not None {response_id}')
                 return
 
-            parse_product(soup, url)
         except Exception as e:
             if e is not MyException:  # might be already logged as error
-                logging.error('in {} was trouble with get-parse {}: {}'.
-                              format(datetime.now().time(), response_id, e))
+                logging.error(f'in {datetime.now().time()} was trouble with get {response_id}: {e}')
             if arguments.debug:
                 # stop and keep calm coding
                 sys.exit(1)
@@ -142,82 +102,102 @@ def get_responce_by_id(response_id):
     mark_tag = soup.find_all('span', 'text-label')
     if mark_tag[0].next_element == 'Оценка:':
         mark = int(mark_tag[0].next_element.next_element.next_element.text)
-    db_elem = {
-        'product': parse_product(soup, url),
-        'bank_name': soup.find('div', tag_bank_name).text.strip('\n\t'),
-        'city': soup.find('div', tag_city).text.split(',')[-1].strip('\n\t'),
-        'title': soup.find('h0', tag_title).text.strip('\n\t'),
-        'mark': mark,
-        'fulltext': soup.find('div', tag_fulltext).text.strip('\n\t'),
-        'datetime': soup.find('time').attrs['datetime']
-    }
-    # print('push', db_elem)
-    db[response_id] = db_elem
+
+    __response = session.query(Response).filter_by(response_id=response_id).first()
+    try:
+        setattr(__response, 'last_update', datetime.now())
+        setattr(__response, 'bank_name', soup.find('div', tag_bank_name).text.strip('\n\t'))
+        setattr(__response, 'city', soup.find('div', tag_city).text.split(',')[-1].strip('\n\t'))
+        setattr(__response, 'title', soup.find('h0', tag_title).text.strip('\n\t'))
+        setattr(__response, 'mark', mark)
+        setattr(__response, 'fulltext', soup.find('div', tag_fulltext).text.strip('\n\t'))
+        setattr(__response, 'datetime', soup.find('time').attrs['datetime'])
+        # dynamic data extracting
+        setattr(__response, 'status', mark_tag[-1].text.strip('\n\t'))
+        setattr(__response, 'num_views', int(soup.find('span', tag_num_views).next_element))
+        setattr(__response, 'num_comments', int(soup.find('span', tag_num_comments).text))
+    except AttributeError:
+        logging.error(f'in {datetime.now().time()} was trouble with get-push {response_id}')
+    session.commit()
 
 
-def update_responce_by_id(response_id):
-    url = 'https://www.banki.ru/services/responses/bank/response/{}/'.format(response_id)
+def update_response_by_id(response_id):
+    url = f'{site_prefix_str}/{resp_suffix_str}/{response_id}/'
     while True:
         try:
             resp = requests.get(url)
-        except:
-            time.sleep(2)
-            continue
+            if resp.status_code == 404:
+                logging.info(f'in {datetime.now().time()} resp.status_code was 404 with {response_id}')
+                return
+            if resp.status_code != 200:
+                logging.error("Error getting %s: %d", url, resp.status_code)
+                return
+            soup = bs4.BeautifulSoup(resp.text, 'lxml')
+            if soup.find('h1', 'header-h0 margin-bottom-large') is not None:
+                logging.info(f'in {datetime.now().time()} header-h0 margin-bottom-large was not None {response_id}')
+                return
+
+        except Exception as e:
+            if e is not MyException:  # might be already logged as error
+                logging.error(f'in {datetime.now().time()} was trouble with get {response_id}: {e}')
+            if arguments.debug:
+                # stop and keep calm coding
+                sys.exit(1)
+            else:
+                # just notify and do as much work as possible
+                time.sleep(2)
+                continue
         else:
             break
-    soup = bs4.BeautifulSoup(resp.text, 'lxml')
-    if soup.find("h1", "header-h0 margin-bottom-large") is None:
-        db[response_id]['status'] = soup.find_all("span", "text-label")[-1].text.strip('\n\t')
-        db[response_id]['num_views'] = int(
-            soup.find("span", "icon-font icon-eye-16 icon-font--size_small").next_element)
-        db[response_id]['num_comments'] = int(soup.find("span", "link-with-icon__text color-gray-blue--alpha-60").text)
-    else:
+
+    if soup.find('h1', 'header-h0 margin-bottom-large') is not None:
+        logging.info(f'in {datetime.now().time()} header-h0 margin-bottom-large was not None {response_id}')
         return
+
+    mark_tag = soup.find_all('span', 'text-label')
+    tag_num_views = 'icon-font icon-eye-16 icon-font--size_small'
+    tag_num_comments = 'link-with-icon__text color-gray-blue--alpha-60'
+
+    print(mark_tag[-1].text.strip('\n\t'), int(soup.find('span', tag_num_views).next_element),int(soup.find('span', tag_num_comments).text))
+    __response2 = session.query(Response).filter_by(response_id=response_id).first()
+    setattr(__response2, 'last_update', datetime.now())
+    setattr(__response2, 'status', mark_tag[-1].text.strip('\n\t'))
+    setattr(__response2, 'num_views', int(soup.find('span', tag_num_views).next_element))
+    setattr(__response2, 'num_comments', int(soup.find('span', tag_num_comments).text))
+    session.commit()
+    return
 
 
 def fetch_response(response_id):
-    if response_id in db:
-        logging.info('in {} start upd id {}'.format(datetime.now().time(), response_id))
-        update_responce_by_id(response_id)
-        logging.info('in {} ended upd id {}'.format(datetime.now().time(), response_id))
+    if session.query(Response.bank_name).filter_by(response_id=response_id).scalar() is not None:
+        logging.info(f'in {datetime.now().time()} start upd id {response_id}')
+        update_response_by_id(response_id)
+        logging.info(f'in {datetime.now().time()}  ended upd id {response_id}')
     else:
-        logging.info('in {} start get id {}'.format(datetime.now().time(), response_id))
-        get_responce_by_id(response_id)
-        logging.info('in {} ended get id {}'.format(datetime.now().time(), response_id))
+        logging.info(f'in {datetime.now().time()}  start get id {response_id}')
+        get_response_by_id(response_id)
+        logging.info(f'in {datetime.now().time()}  ended get id {response_id}')
 
 
-set_ids = set()
-input_file = arguments.input
-min_id = arguments.min_id
-for line in input_file:
-    _id = int(line)
-    if _id > min_id:  # меньшие этого id нас не интересуют
-        set_ids.add(_id)
-new_ids = list(set_ids)
+engine = create_engine(db_string, pool_recycle=60)
+Session = sessionmaker(bind=engine)
+session = Session()
+new_ids = [result.response_id for result in session.query(Response.response_id).filter_by(**{'bank_name': None})]
 
-db = {}
-if arguments.max_count is not None:
-    new_ids = new_ids[:arguments.max_count]
+start = datetime.now()
+for elem in new_ids:
+    # print(elem)
+    fetch_response(elem)
+end = datetime.now()
+print(end-start)
 
-with ThreadPoolExecutor(max_workers=4) as executor:
-    for _ in executor.map(fetch_response, new_ids):
-        pass
+# with ThreadPoolExecutor(max_workers=4) as executor:
+#     for _ in executor.map(fetch_response, new_ids):
+#         pass
 
-
-def update_entry_products(entry: dict):
-    for prod in all_product_types:
-        field_name = "prod_" + prod
-        entry[field_name] = 1 if prod in entry["product"] else 0
-    del entry["product"]
-
-
-for entry in db.values():
-    update_entry_products(entry)
-
-print(len(db))
-
-df = DataFrame(db)
-transposed_df = df.T
-pickle.dump(transposed_df, arguments.output, protocol=4)
-if arguments.gzip_output:
-    subprocess.run(["gzip", "-9", arguments.output.name])
+#
+# df = DataFrame(db)
+# transposed_df = df.T
+# pickle.dump(transposed_df, arguments.output, protocol=4)
+# if arguments.gzip_output:
+#     subprocess.run(["gzip", "-9", arguments.output.name])
