333dfde0fc786d737d433125c26edf41f4359501
sir_polin <polin@phystech.edu>
2020-06-06 20:54:15 +0300

    ThreadPool work fixed

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import requests
 # ! pip3 install pandas
 from pandas import DataFrame
 # ! pip3 install sqlalchemy, psycopg2
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import create_engine
+from contextlib import contextmanager
+
 
 # standard library modules
 import logging
@@ -61,7 +63,8 @@ with open('_site_prefix', 'r') as site_prefix_file:
         db_string = db_string_file.readline()
 
 
-def get_response_by_id(response_id):
+def fetch_response(session, response_id):
+    logging.info(f'in {datetime.now().time()}  start get/upd id {response_id}')
     tag_bank_name = 'header-h2 display-inline margin-right-x-small'
     tag_city = 'response-page__bank-meta font-size-medium color-gray-burn'
     tag_title = 'header-h0 response-page__title'
@@ -97,7 +100,6 @@ def get_response_by_id(response_id):
         else:
             break
 
-    # (soup.find("h1", "header-h0 margin-bottom-large").text == 'Ошибка')
     mark = 0
     mark_tag = soup.find_all('span', 'text-label')
     if mark_tag[0].next_element == 'Оценка:':
@@ -105,95 +107,56 @@ def get_response_by_id(response_id):
 
     __response = session.query(Response).filter_by(response_id=response_id).first()
     try:
-        setattr(__response, 'last_update', datetime.now())
-        setattr(__response, 'bank_name', soup.find('div', tag_bank_name).text.strip('\n\t'))
-        setattr(__response, 'city', soup.find('div', tag_city).text.split(',')[-1].strip('\n\t'))
-        setattr(__response, 'title', soup.find('h0', tag_title).text.strip('\n\t'))
-        setattr(__response, 'mark', mark)
-        setattr(__response, 'fulltext', soup.find('div', tag_fulltext).text.strip('\n\t'))
-        setattr(__response, 'datetime', soup.find('time').attrs['datetime'])
+        if session.query(Response.bank_name).filter_by(response_id=response_id).scalar() is None:
+            setattr(__response, 'last_update', datetime.now())
+            setattr(__response, 'bank_name', soup.find('div', tag_bank_name).text.strip('\n\t'))
+            setattr(__response, 'city', soup.find('div', tag_city).text.split(',')[-1].strip('\n\t'))
+            setattr(__response, 'title', soup.find('h0', tag_title).text.strip('\n\t'))
+            setattr(__response, 'mark', mark)
+            setattr(__response, 'fulltext', soup.find('div', tag_fulltext).text.strip('\n\t'))
+            setattr(__response, 'datetime', soup.find('time').attrs['datetime'])
         # dynamic data extracting
         setattr(__response, 'status', mark_tag[-1].text.strip('\n\t'))
         setattr(__response, 'num_views', int(soup.find('span', tag_num_views).next_element))
         setattr(__response, 'num_comments', int(soup.find('span', tag_num_comments).text))
     except AttributeError:
         logging.error(f'in {datetime.now().time()} was trouble with get-push {response_id}')
-    session.commit()
-
-
-def update_response_by_id(response_id):
-    url = f'{site_prefix_str}/{resp_suffix_str}/{response_id}/'
-    while True:
-        try:
-            resp = requests.get(url)
-            if resp.status_code == 404:
-                logging.info(f'in {datetime.now().time()} resp.status_code was 404 with {response_id}')
-                return
-            if resp.status_code != 200:
-                logging.error("Error getting %s: %d", url, resp.status_code)
-                return
-            soup = bs4.BeautifulSoup(resp.text, 'lxml')
-            if soup.find('h1', 'header-h0 margin-bottom-large') is not None:
-                logging.info(f'in {datetime.now().time()} header-h0 margin-bottom-large was not None {response_id}')
-                return
-
-        except Exception as e:
-            if e is not MyException:  # might be already logged as error
-                logging.error(f'in {datetime.now().time()} was trouble with get {response_id}: {e}')
-            if arguments.debug:
-                # stop and keep calm coding
-                sys.exit(1)
-            else:
-                # just notify and do as much work as possible
-                time.sleep(2)
-                continue
-        else:
-            break
-
-    if soup.find('h1', 'header-h0 margin-bottom-large') is not None:
-        logging.info(f'in {datetime.now().time()} header-h0 margin-bottom-large was not None {response_id}')
-        return
-
-    mark_tag = soup.find_all('span', 'text-label')
-    tag_num_views = 'icon-font icon-eye-16 icon-font--size_small'
-    tag_num_comments = 'link-with-icon__text color-gray-blue--alpha-60'
-
-    print(mark_tag[-1].text.strip('\n\t'), int(soup.find('span', tag_num_views).next_element),int(soup.find('span', tag_num_comments).text))
-    __response2 = session.query(Response).filter_by(response_id=response_id).first()
-    setattr(__response2, 'last_update', datetime.now())
-    setattr(__response2, 'status', mark_tag[-1].text.strip('\n\t'))
-    setattr(__response2, 'num_views', int(soup.find('span', tag_num_views).next_element))
-    setattr(__response2, 'num_comments', int(soup.find('span', tag_num_comments).text))
-    session.commit()
-    return
-
-
-def fetch_response(response_id):
-    if session.query(Response.bank_name).filter_by(response_id=response_id).scalar() is not None:
-        logging.info(f'in {datetime.now().time()} start upd id {response_id}')
-        update_response_by_id(response_id)
-        logging.info(f'in {datetime.now().time()}  ended upd id {response_id}')
-    else:
-        logging.info(f'in {datetime.now().time()}  start get id {response_id}')
-        get_response_by_id(response_id)
-        logging.info(f'in {datetime.now().time()}  ended get id {response_id}')
+    logging.info(f'in {datetime.now().time()}  ended get-upd id {response_id}')
 
 
 engine = create_engine(db_string, pool_recycle=60)
-Session = sessionmaker(bind=engine)
-session = Session()
-new_ids = [result.response_id for result in session.query(Response.response_id).filter_by(**{'bank_name': None})]
+Main_Session = sessionmaker(bind=engine)
+main_session = Main_Session()
+new_ids = [result.response_id for result in main_session.query(Response.response_id).filter_by(**{'bank_name': None})]
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session_factory = sessionmaker(bind=engine)
+    Session = scoped_session(session_factory)
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def thread_worker(response_id):
+    # We're using the session context here.
+    with session_scope() as session:
+        fetch_response(session, response_id)
+
 
 start = datetime.now()
-for elem in new_ids:
-    # print(elem)
-    fetch_response(elem)
+with ThreadPoolExecutor(max_workers=8) as executor:
+    for _ in executor.map(thread_worker, new_ids):
+        pass
 end = datetime.now()
 print(end-start)
-
-# with ThreadPoolExecutor(max_workers=4) as executor:
-#     for _ in executor.map(fetch_response, new_ids):
-#         pass
 
 #
 # df = DataFrame(db)
